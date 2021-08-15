4e64a964a6d7b9f8fa24880b93532a13a8666424
raghavgarg188 <77096544+raghavgarg188@users.noreply.github.com>
2021-08-15 15:51:39 +0530

    Add files via upload

---
+++ food.js
@@ -0,0 +1,41 @@
+class Food{
+constructor(){
+    this.foodStock = 0;
+    this.lastfed
+    this.image = loadImage('Images/Milk.png');
+
+}
+
+updateFoodStock(foodStock){
+    this.foodStock = foodStock;
+
+}
+deductFood(){
+    if(this.foodStock>0){
+        this.foodStock = this.foodStock-1
+
+    }
+}
+
+getFoodStock(){
+    return this.foodStock
+}
+display(){
+    var X = 80;
+    var Y = 100;
+    imageMode(CENTRE);
+    image(this.image,720,220,70,70);
+    if(this.foodStock!=0){
+        for(var i = 0;i<this.foodStock;i++){
+            if(i%10 == 0){
+                X = 80;
+                Y = Y+50;
+
+            }
+            image(this.image,x,y,50,50);
+            x = x+50
+        }
+    }
+}
+
+}
